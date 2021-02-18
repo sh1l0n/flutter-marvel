7c9cedd66657f1db7ee45452db5e8a856c8c1f85
chancla15 <jesusmanresa1@gmail.com>
2021-02-18 20:38:02 +0100

    wraped series

--- libs/lib_marvel/lib/marvel_api.dart
+++ libs/lib_marvel/lib/marvel_api.dart
@@ -7,6 +7,18 @@ import 'package:crypto/crypto.dart';
 // import 'package:http/http.dart' as http;
 
 
+class MarvelSerieWrapper {
+  const MarvelSerieWrapper(this.id, this.title, this.imagePath);
+  final int id;
+  final String title;
+  final String imagePath;
+  Map<String, dynamic> toJson() => {'id': id, 'title': title, 'imagePath': imagePath};
+  @override
+  String toString() {
+    return jsonEncode(toJson());
+  }
+}
+
 class MarvelApiWrapper {
   const MarvelApiWrapper(this.secret, this.public);
   final String secret;
@@ -63,7 +75,60 @@ class MarvelApi {
 
     // String url2 = "https://jsonplaceholder.typicode.com/posts"; 
     final response = await http.get(url, headers: headers);
-    print(response.body); 
+
+    /*
+      final result = results[0];
+      final id = result[id] as int;
+      final title = result['title'];
+      final thumbnail = results['thumbnail'];
+
+    */
+
+    /*
+    {id: 26024, title:  Superior Spider-Man Vol. 2: Otto-matic (2019), description: null, 
+    resourceURI: http://gateway.marvel.com/v1/public/series/26024, urls: [{type: detail, url:
+     http://marvel.com/comics/series/26024/_superior_spider-man_vol_2_otto-matic_2019?utm_campaign=apiRef&utm_source=
+     00545860849df16b919db440f55b040d}], startYear: 2019, endYear: 2019, rating: , type: collection, modified: 2019-12-13T16:23:45-0500, 
+     thumbnail: {path: http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available, extension: jpg}, creators: {available: 4, 
+     collectionURI: http://gateway.marvel.com/v1/public/series/26024/creators, 
+     items: [{resourceURI: http://gateway.marvel.com/v1/public/creators/11765, name: Christos Gage, role: writer}, 
+     {resourceURI: http://gateway.marvel.com/v1/public/creators/942, name: Mike Hawthorne, role: penciller (cover)},
+      {resourceURI: http://gateway.marvel.com/v1/public/creators/437, name: Lan Medina, role: penciller}, 
+      {resourceURI: http://gateway.marvel.com/v1/public/creators/4430, name: Jeff Youngquist, role: editor}], returned: 4},
+       characters: {available: 1, collectionURI: http://gateway.marvel.com/v1/public/series/26024/characters, 
+       items: [{resourceURI: http://gateway.marvel.com/v1/public/characters/1009610, name: Spider-Man}], returned: 1}, 
+       stories: {available: 2, collectionURI: http://gateway.marvel.com/v1/public/series/26024/stories, 
+       items: [{resourceURI: http://gateway.marvel.com/v1/public/stories/158776, name: cover from SUPERIOR SPIDER-MAN VOL. 2 TPB (2020) #2,
+        type: cover}, {resourceURI: http://gateway.marvel.com/v1/public/stories/158777, name: story from SUPERIOR SPIDER-MAN VOL. 2 TPB (2020) #2, 
+        type: interiorStory}], returned: 2}, comics: {available: 1, collectionURI: http://gateway.marvel.com/v1/public/series/26024/comics, 
+        items: [{resourceURI: http://gateway.marvel.com/v1/public/comics/71400, name:  Superior Spider-Man Vol. 2: Otto-matic (Trade Paperback)}], 
+        returned: 1}, events: {available: 0, collectionURI: http://gateway.marvel.com/v1/public/series/26024/events, items: [], returned: 0}, 
+        next: null, previous: null}
+
+    */
+
+    List<MarvelSerieWrapper> series = [];
+    
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> jsonData = jsonDecode(response.body);
+      final data = jsonData['data'] as Map<String, dynamic>;
+      final results = data['results'] as List;
+
+
+      results.forEach((final result) { 
+        final id = result['id'] as int;
+        final title = result['title'] as String;
+        final thumbnail = result['thumbnail'] as Map<String, dynamic>;
+        final imagePath = thumbnail['path'] as String;
+        final imageExtension = thumbnail['extension'] as String;
+        final imageUrl = imagePath + '.' + imageExtension;
+        series += [MarvelSerieWrapper(id, title, imageUrl)];
+      });
+     
+      print('series: $series');
+      print(results.length);
+    }
+    
 
     // final request = await HttpClient().getUrl(Uri.parse(url));
     // print('request: $request');
